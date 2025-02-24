0507257791423c312686092b20fe631a49f7901c
SethmiDanoshi <111180840+SethmiDanoshi@users.noreply.github.com>
2025-02-24 23:26:03 +0530

    Add About section

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import Carousel from 'react-bootstrap/Carousel';
+import About from './About';
 
 function HomePage() {
   return (
+    <>
+    {/*Carousel */}
     <Carousel className="w-full h-screen">
       {/* First Slide */}
       <Carousel.Item interval={1000} className="w-full h-screen relative">
@@ -114,6 +117,27 @@ function HomePage() {
       </Carousel.Item>
 
     </Carousel>
+
+{/* About */}
+
+    <div className="flex flex-col items-center justify-center min-h-screen bg-white p-6">
+      <h1 className="text-6xl font-bold text-pink-600" style={{ fontFamily: 'Irish Grover' }}>PetMate</h1>
+      <h2 className="text-2xl font-semibold text-purple-800 mt-2" style={{ fontFamily: 'Jacques Francois' }}>
+        The innovative Pet Tech solution for pet lovers.
+      </h2>
+      <p className="text-black text-lg text-center max-w-2xl mt-4" style={{ fontFamily: 'Jacques Francois' }}>
+        PetMate is a unified platform designed to address the challenges faced by
+        pet lovers in Sri Lanka. It streamlines the process of buying and selling
+        pets, and accessing pet care items and services. This web-based platform
+        simplifies pet ownership, saving time and money while enhancing the overall
+        experience for pet owners, making pet care more enjoyable and efficient.
+      </p>
+      <p className="text-4xl  mt-6" style={{ color: "#66D1EE" , fontFamily: 'Irish Grover'}}>
+        " Embrace the future of pet care with PetPaw Commerce "
+      </p>
+      <img src="Pet.png" alt="PetMate Banner" className="w-full max-w-4xl mt-6" />
+    </div>
+   </> 
   );
 }
 
